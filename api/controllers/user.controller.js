c37dc2f8459226f68e2e76314e02cf80f2bc633e
adimishra6003 <adityamishra120799@gmail.com>
2024-03-23 21:51:58 +0530

    created a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+const test = (req, res)=>{
+    res.json({message:"API hit!"});
+}
+
+module.exports = test;
+
--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-const { log } = require('console');
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoutes = require('./routes/user.route.js')
+// import userRoutes from './routes/user.route.js'
 
 dotenv.config();
 
@@ -11,11 +12,12 @@ mongoose.connect(process.env.MONGO)
 }).catch((err)=>{
     console.log(err);
 })
-//Username-adityamishra120799
-//Password-VUV3CNGaV8n5Fpag
+
 
 const app = express();
 
 app.listen(3000, ()=>{
     console.log('Server is running on PORT:3000!');
 })
+
+app.use('/api/user', userRoutes)

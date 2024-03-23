a3566cd83c098f2c71bf70ee22183b3b6655dfbd
adimishra6003 <adityamishra120799@gmail.com>
2024-03-23 19:37:48 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
+const { log } = require('console');
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("MongoDB connected!");
+}).catch((err)=>{
+    console.log(err);
+})
+//Username-adityamishra120799
+//Password-VUV3CNGaV8n5Fpag
 
 const app = express();
 

be6e16db404d7cd094ccfd845a0b3589a23a1a8d
adimishra6003 <adityamishra120799@gmail.com>
2024-03-27 01:18:13 +0530

    Created signin api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const signup = require('../controllers/auth.controller.js')
+const {signin, signup} = require('../controllers/auth.controller.js')
 
 const router = express.Router();
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 module.exports = router;

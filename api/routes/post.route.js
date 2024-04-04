3661e6bfcf852e26887f4108e52d58bc7704fcbc
adimishra6003 <adityamishra120799@gmail.com>
2024-04-05 03:30:15 +0530

    added update post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {verifyUser} = require('../utils/verifyUser.js')
-const {create, getposts, deletepost} = require('../controllers/post.controller.js')
+const {create, getposts, deletepost, updatepost} = require('../controllers/post.controller.js')
 
 router.post('/create', verifyUser, create);
 router.get('/getposts', getposts)
 router.delete('/deletepost/:postId/:userId', verifyUser, deletepost);
+router.put('/updatepost/:postId/:userId', verifyUser, updatepost);
 
 module.exports = router;

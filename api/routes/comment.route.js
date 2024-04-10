345badbc5feceac2988320b1f6bb31d6d8ab656c
adimishra6003 <adityamishra120799@gmail.com>
2024-04-11 02:09:58 +0530

    added delete functionality to comments section

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {verifyUser}  = require('../utils/verifyUser.js')
-const {createComment, getPostComments, likeComment, editComment} = require('../controllers/comment.controller.js')
+const {createComment, getPostComments, likeComment, editComment, deleteComment} = require('../controllers/comment.controller.js')
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.post('/create', verifyUser, createComment);
 router.get('/getPostComments/:postId', getPostComments)
 router.put('/likeComment/:commentId', verifyUser, likeComment)
 router.put('/editComment/:commentId', verifyUser, editComment)
+router.delete('/deleteComment/:commentId', verifyUser, deleteComment)
 
 module.exports = router;

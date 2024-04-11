743a939893bc97b2e5e72a731614db34918648ac
adimishra6003 <adityamishra120799@gmail.com>
2024-04-11 21:19:00 +0530

    completed about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,50 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Aditya's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              "Welcome to Aditya's Blog, your go-to destination for insightful
+              articles on technology, software engineering, and web development.
+              Founded with a passion for sharing knowledge and experiences in
+              the fast-paced world of technology, our platform aims to empower
+              and inspire developers, enthusiasts, and learners alike. Whether
+              you're diving into the latest trends, exploring cutting-edge
+              technologies, or seeking practical advice for your coding journey,
+              Aditya's Blog is here to guide you through the ever-evolving
+              landscape of the digital world."
+            </p>
+            <p>
+              "At Aditya's Blog, we believe in the transformative power of
+              education and information sharing. With a focus on clarity, depth,
+              and relevance, our articles strive to demystify complex topics,
+              offer practical insights, and spark meaningful conversations
+              within the tech community. From beginner-friendly tutorials to
+              in-depth analyses of industry trends, our diverse range of content
+              caters to readers of all skill levels and interests. Join us on a
+              journey of exploration, discovery, and continuous learning as we
+              navigate the exciting world of technology together."
+            </p>
+            <p>
+              "Driven by a relentless pursuit of excellence and a commitment to
+              fostering a culture of collaboration and growth, Aditya's Blog is
+              more than just a platform for sharing articles — it's a community.
+              We invite you to join our community of tech enthusiasts, thought
+              leaders, and innovators as we explore the latest developments,
+              share valuable insights, and celebrate the boundless possibilities
+              of technology. Together, let's embark on a journey of discovery,
+              inspiration, and innovation as we shape the future of technology,
+              one article at a time."
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import CallToAction from "../components/CallToAction.jsx";
 
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        "Welcome to the Projects page of Aditya's Blog, your gateway to hands-on
+        learning and practical application of technology. Here, you'll discover
+        a curated collection of innovative projects spanning various domains,
+        from web development to artificial intelligence. Each project is
+        carefully selected to provide valuable insights, real-world challenges,
+        and opportunities for experimentation. Whether you're a seasoned
+        developer looking to expand your skills or a newcomer eager to dive into
+        the world of coding, our projects offer a rich learning experience
+        tailored to your needs. Explore, experiment, and embark on your coding
+        journey with confidence as you tackle exciting projects and unlock your
+        full potential with Aditya's Blog."
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
